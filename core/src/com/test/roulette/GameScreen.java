8c6bd940d89954524ee96a8e295a0b9930fce13e
Joaco99 <viscafe38@gmail.com>
2017-07-22 19:19:52 -0400

    Ficed Classes

--- core/src/com/test/roulette/GameScreen.java
+++ core/src/com/test/roulette/GameScreen.java
@@ -2,6 +2,7 @@ package com.test.roulette;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -34,8 +35,11 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
 
-        renderer.begin(ShapeRenderer.ShapeType.Filled);
-
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        renderer.setColor(Color.RED);
+        renderer.arc(200, 200, 100, 0, 180, 100);
+        renderer.setColor(Color.GREEN);
+        renderer.arc(200, 200, 100, 180, 180, 100);
         renderer.end();
 
         batch.begin();
