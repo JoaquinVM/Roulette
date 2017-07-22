ddb9bfbb3a9708c0b393504ed33e7fce95b8fd6e
Groxar <lrdolucho@gmail.com>
2017-07-22 18:48:36 -0400

    some constants

---
+++ core/src/com/test/roulette/Constants.java
@@ -0,0 +1,14 @@
+package com.test.roulette;
+
+/**
+ * Created by Luchox on 22/07/2017.
+ */
+
+public class Constants {
+
+    public static final int WORLD_WIDTH = 600;
+    public static final int WORLD_HEIGHT = 400;
+    public static final int SPINS = 10;
+    public static final float BUTTON_SIZE = WORLD_HEIGHT / 10;
+
+}
--- core/src/com/test/roulette/Roulette.java
+++ core/src/com/test/roulette/Roulette.java
@@ -1,17 +1,13 @@
 package com.test.roulette;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Roulette extends Game {
-	
-	@Override
-	public void create () {
-	}
+
+    @Override
+    public void create() {
+        setScreen(new GameScreen());
+    }
 
 
 }
