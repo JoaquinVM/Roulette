f7223e435bbef8063d2409cb8326901e15f0f271
Joaco99 <viscafe38@gmail.com>
2017-07-22 19:10:39 -0400

    Ficed Classes

--- core/src/com/test/roulette/GameScreen.java
+++ core/src/com/test/roulette/GameScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
@@ -16,12 +17,14 @@ public class GameScreen extends ScreenAdapter {
     Stage stage;
     SpriteBatch batch;
     BitmapFont font;
+    private ShapeRenderer renderer;
 
     @Override
     public void show() {
         stage = new Stage();
         batch = new SpriteBatch();
         font = new BitmapFont();
+        renderer = new ShapeRenderer();
 
         stage.getViewport().setWorldSize(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
     }
@@ -30,6 +33,11 @@ public class GameScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
+
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        renderer.end();
+
         batch.begin();
         stage.draw();
         batch.end();
@@ -45,5 +53,6 @@ public class GameScreen extends ScreenAdapter {
         stage.dispose();
         batch.dispose();
         font.dispose();
+        renderer.dispose();
     }
 }
