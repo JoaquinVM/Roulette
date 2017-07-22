f04fb2671076d034b7c8526598f5d875bc94abbf
Joaco99 <viscafe38@gmail.com>
2017-07-22 18:19:25 -0400

    Initial commit

---
+++ desktop/src/com/test/roulette/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.test.roulette.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.test.roulette.Roulette;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Roulette(), config);
+	}
+}
